95d33e7187550e1f9136f56bb6ca72bd1672fc6e
Anton <anton_real_madrid@icloud.com>
2019-07-16 23:00:45 +0200

    first push to dev

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // dev push
   final _questions = const [
     {
       'questionText': 'What\'s your favorite game?',
